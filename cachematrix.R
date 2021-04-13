eca99cc8aad703e3138b00ecae95ddd603af5391
KryzaCare <82439630+KryzaCare@users.noreply.github.com>
2021-04-13 21:12:55 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,39 @@
-## Doing the Matrix Inversion usually takes a long hussle and needs more time since 
-## it requires a repeated computation 
-## Here are the prepared pair of functions to be used that will 
-## served as the special object that will basically store the matrix and 
-## caches its inverse
+## Taking the course - R Programming in coursera includes assignments
+## such as Lexical Scoping. 
 
-## BAsically, this function will have a special matrix object for it to caches its inverse
+## In this there will be technical descriptions or comments that will explain how the functions was used. 
+## The functions were encoded below. 
 
-makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y) {
+## Since the functions involves repetitive computations, this normally takes a long time and requires more time
+## BAsically, this fucntion will have a special matrix object for it to caches k
+
+makeCacheMatrix <- function(x = matrix()) {			## Shows the default mode of "matrix" used to determine the point. 
+        k <- NULL						## k will serve as NULL and will keep the value of the matrix mean
+        set <- function(y) {					## When the new matrix is identified then you have to reset k to NULL and assign it to new
                 x <<- y
-                inv <<- NULL
+                k <<- NULL
         }
-        get <- function() x
-                setInverse <- function (inverse) inv <<- inverse
-        getInverse <- function() inv
-        list(set = set,
-             get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
+        get <- function() x					## Defines the function for taking the frequency
+                setC <- function (mean) k <<- mean		## The value of mean will be assigned
+        getC <- function() k					## Shows the function to calculate the mean
+        list(set = set, get = get,				
+             setC = setC,
+             getC = getC)
 }
 
-## In this part, this function computes already the inverse of the special matrix that was 
-## mentioned and done above. 
-## So, when the inverse is already calculated but then the matrix has not changed then 
-##probably, the inverse should be retrieved from the cache. 
+## In this part, it already computes the mean of the special matrix that was stated above. 
+## So, if the mean has already been computed but the matrix hasn't modified, the mean can
+## most likely be retrieved from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getInverse()
-        if (!is.null(inv)) {
+        ## Return a matrix that is the mean of 'x'
+        k <- x$getmean()
+        if (!is.null(k)) {
                 message ("getting cached data")
-                return(inv)
+                return(k)
         }
-        mat <- x$get()
-        inv <- solve(mat, ...)
-        x$setInverse(inv)
-        inv
+        mtrx <- x$get()
+        k <- solve(mtrx, ...)
+        x$setC(k)
+        k
 }
